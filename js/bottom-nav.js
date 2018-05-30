7d015f1c70a54d8cd19ee09b629d30b40446c676
NorthSouth <nschnepf@hotmail.com>
2018-05-29 21:56:05 -0700

    housecleaning

--- js/bottom-nav.js
+++ js/bottom-nav.js
@@ -1,5 +1,5 @@
 bottomNav = document.querySelector('.bottom-nav');
-richArea = document.querySelector('.rich-area');
+menuArea = document.querySelector('.menu-area');
 slideTrigger = document.querySelector('.slide-trigger');
 slideTriggerLink = document.querySelector('.slide-trigger-link');
 bottomNavHeader = document.querySelector('.bottom-nav-header');
@@ -13,19 +13,19 @@ function slideUp() {
   if (slideTrigger.classList.contains('active')) {
     /*bottomNav.style.height = '100%';*/
     bottomNav.style.height = '20%';
-    richArea.style.opacity = '1'; 
+    menuArea.style.opacity = '1'; 
     slideTrigger.children[0].style.transformOrigin = '50% 50%';
     slideTrigger.children[0].style.transform = 'rotate(180deg)';
     bottomNavHeader.style.color='rgba(255,255,255,1)';
     bottomNav.addEventListener('transitionend', () => {
-      richArea.style.opacity = '1'
+      menuArea.style.opacity = '1'
     })
   } else {
     bottomNav.style.height = '48px';
     slideTrigger.children[0].style.transform = 'rotate(0deg)';
     bottomNavHeader.style.color='rgba(255,255,255,.5)';
     bottomNav.addEventListener('transitionend', () => {
-      richArea.style.opacity = '0'
+      menuArea.style.opacity = '0'
     });
   }
 }
